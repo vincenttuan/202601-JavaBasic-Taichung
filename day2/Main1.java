8bf31856e8ad1d92f2f8473d7100722e8830b1a5
VincentTuan <vincentjava@yahoo.com.tw>
2026-01-10 10:09:39 +0800

    Add files via upload

---
+++ day2/Main1.java
@@ -0,0 +1,20 @@
+package day2;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 3;
+		int sum = x + y; // 加法 = 8
+		int sub = x - y; // 減法 = 2
+		int mul = x * y; // 乘法 = 15
+		int div = x / y; // 除法 = 1 而非 1.6666
+		int mod = x % y; // 餘數 = 2
+		System.out.println(x + " + " + y + " = " + sum);
+		System.out.println(x + " - " + y + " = " + sub);
+		System.out.println(x + " * " + y + " = " + mul);
+		System.out.println(x + " / " + y + " = " + div);
+		System.out.println(x + " % " + y + " = " + mod);
+	}
+
+}
