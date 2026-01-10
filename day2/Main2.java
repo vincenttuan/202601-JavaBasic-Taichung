14d9cf4d296f65da539bb1c3f4974a23799a13a6
VincentTuan <vincentjava@yahoo.com.tw>
2026-01-10 10:36:46 +0800

    Add files via upload

---
+++ day2/Main2.java
@@ -0,0 +1,24 @@
+package day2;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		/*
+		 * 可樂一瓶: 25 元
+		 * 一手有: 6 瓶
+		 * 一打有: 12 瓶
+		 * 一箱有: 24 瓶
+		*/
+		int cola = 25; // 可樂一瓶的價格
+		int hand = 6;  // 一手有幾瓶
+		int doz  = 12; // 一打有幾瓶
+		int box  = 24; // 一箱有幾瓶
+		
+		// 買 10 箱要多少錢 ?
+		int sum = cola * box * 10;
+		
+		System.out.printf("可樂一瓶 %d 元 買 10 箱共需 %d 元%n", cola, sum);
+
+	}
+
+}
