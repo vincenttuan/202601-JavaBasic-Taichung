2877858440bef046e579f746e7bde6969194ecdb
VincentTuan <vincentjava@yahoo.com.tw>
2026-01-10 15:57:26 +0800

    Add files via upload

---
+++ day2/Main10.java
@@ -0,0 +1,33 @@
+package day2;
+
+import java.util.Scanner;
+
+public class Main10 {
+
+	public static void main(String[] args) {
+		/*
+			使用者可以輸入成績, 系統根據成績印出 level 
+			score >= 90 得 A
+			score >= 80 && score < 90 得 B
+			score >= 70 && score < 80 得 C
+			score >= 60 && score < 70 得 D
+			score <  60 得 F
+		*/
+		Scanner sc = new Scanner(System.in);
+		System.out.printf("請輸入成績:");
+		int score = sc.nextInt();
+		
+		// 判斷 level
+		if(score >= 90) {
+			System.out.println("得 A");
+		} else if(score >= 80 && score < 90) {
+			System.out.println("得 B");
+		} else if(score >= 70 && score < 80) {
+			System.out.println("得 C");
+		} else if(score >= 60 && score < 70) {
+			System.out.println("得 D");
+		} else {
+			System.out.println("得 F");
+		}
+	}
+}
