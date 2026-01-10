3027c7ab3d6ef5a8e01a06b70e343c6934da3736
VincentTuan <vincentjava@yahoo.com.tw>
2026-01-10 14:16:40 +0800

    Update Main7.java

--- day2/Main7.java
+++ day2/Main7.java
@@ -11,6 +11,24 @@ public class Main7 {
 		int change = cash - cola; // 找錢
 
 		System.out.printf("可樂一瓶 %d 元 付款 %d 元 應找 %d 元%n", cola, cash, change);
-	}
 
+		// 找零錢 50 元, 此時的 change = 77
+		int fifty = change / 50; // -> 77 / 50 = 1
+		System.out.printf("50元: %d 個%n", fifty);
+		change = change % 50; // 還剩下多少錢沒找 -> 77 % 50 = 27
+
+		// 找零錢 10 元, 此時的 change = 27
+		int ten = change / 10; // -> 27 / 10 = 2
+		System.out.printf("10元: %d 個%n", ten);
+		change = change % 10; // -> 27 % 10 = 7
+
+		// 找零錢 5 元, 此時的 change = 7
+		int five = change / 5; // -> 7 / 5 = 1
+		System.out.printf(" 5元: %d 個%n", five);
+		change = change % 5; // -> 7 % 5 = 2
+
+		// 找零錢 1 元, 此時的 change = 2
+		int one = change;
+		System.out.printf(" 1元: %d 個%n", one);
+	}
 }
