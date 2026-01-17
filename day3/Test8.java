5a61dc47d77127da04c92fb6eb9c45f5c3f8e887
VincentTuan <vincentjava@yahoo.com.tw>
2026-01-17 12:10:35 +0800

    Add files via upload

---
+++ day3/Test8.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class Test8 {
+
+	public static void main(String[] args) {
+		
+		// 利用 for 迴圈印出 10 個 Java
+		for(int x=1 ; x <= 10 ; x++) {
+			System.out.printf("第 %02d 個 Java%n", x);
+		}
+
+	}
+
+}
