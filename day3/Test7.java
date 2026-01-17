a623000add56310ef61d650a19787bac18bb2bec
VincentTuan <vincentjava@yahoo.com.tw>
2026-01-17 11:46:57 +0800

    Add files via upload

---
+++ day3/Test7.java
@@ -0,0 +1,35 @@
+package day3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Test7 {
+
+	// 1~9 猜一個數字
+	public static void main(String[] args) {
+		Random r = new Random();
+		Scanner sc = new Scanner(System.in);
+
+		int ans = 8;
+
+		do {
+			System.out.printf("請輸入 1~9 數字 ==> ");
+			int guess = sc.nextInt();
+			if(guess > ans) {
+				System.out.println("猜大了");
+				continue;
+			} else if(guess < ans) {
+				System.out.println("猜小了");
+				continue;
+			} else {
+				System.out.println("猜對了");
+				break;
+			}
+
+		} while(true);
+
+		System.out.println("Game over !");
+
+	}
+
+}
